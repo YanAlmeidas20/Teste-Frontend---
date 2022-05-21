c19a1370105bde0d2c5bb107468087172aed1c4d
github_username <yansantos324@gmail.com>
2022-05-21 01:23:15 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+ 
+ <v-app>
+     <header/>
+     <router-view path="$router.key"/>
+  </v-app>
+</template>
+
+<script>
+import Hearder from './components/Hearder.vue';
+import './styles/global.css'
+export default {
+  name: 'App',
+  components: {
+    Hearder,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
